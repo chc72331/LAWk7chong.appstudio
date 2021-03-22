095b6fbce0e36baef46eac5afffa99319de57e01
chc72331 <chc72331@creighton.edu>
2021-03-22 14:07:35 -0500

    Initial commit

---
+++ forms/customerSelect/customerSelect.js
@@ -0,0 +1,7 @@
+// global variables for database calls
+let req = ""
+let query = ""
+let results = ""
+let pw = "ac@T!.........."  // put your database password here
+let netID = "chc72331"
+let allCustomersData = []
