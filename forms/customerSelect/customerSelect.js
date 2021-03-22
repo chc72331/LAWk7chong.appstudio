60791d4af852fb2b0ee69d6960ee69255a51b2d7
chc72331 <chc72331@creighton.edu>
2021-03-22 15:03:02 -0500

    Update customerSelect.js

--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -1,7 +1,30 @@
-// global variables for database calls
-let req = ""
+// Global variables for database calls
+let req = {} // Indicates empty object
 let query = ""
-let results = ""
+let results = []
 let pw = "ac@T!.........."  // put your database password here
 let netID = "chc72331"
-let allCustomersData = []
+
+
+customerSelect.onshow = function(){
+  query = "SELECT * FROM customers"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  
+  if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`The results are \n ${results}`)
+        if (results.length == 0)    
+           lblMessage.value = "There are no customers in the database."
+        else {        
+           let message = ""
+           for (i = 0; i < results.length; i++)
+               message = message + results[i][1] + "\n"
+           txtaCustomerList.value = message
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessage.value = "Error code: " + req.status
+
+}
+
+
