1f5bba24290909f5d82ea809d2124e93de75c0e3
chc72331 <chc72331@creighton.edu>
2021-03-22 21:52:31 -0500

    Update customerDelete.js

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -1,20 +1,42 @@
-customerDelete.onshow = function(){
-  query = "SELECT * FROM customers"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-  
-  if (req.status == 200) { //transit trip worked. 
-        results = JSON.parse(req.responseText)
-        console.log(`The results are \n ${results}`)
-        if (results.length == 0)    
-           lblMessage2.value = "There are no customers in the database."
-        else {        
-           let message = ""
-           for (i = 0; i < results.length; i++)
-               message = message + results[i][1] + "\n"
-           txtaCustomer2.value = message
-        } // end else
+customerDelete.onshow=function(){
+     // get all the pet data from the database when program loads
+    query = "SELECT * FROM customers"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
 
-    } else   // the transit didn't work - bad wifi? server turned off?
-        lblMessages2.value = "Error code: " + req.status
+    if (req.status == 200) { //transit worked.
+        allCustomerData = JSON.parse(req.responseText)  // parse data in an array
+        //console.log(allCustomerData)
+        let message = ""
+        for (i = 0; i < allCustomerData.length; i++)
+               message = message + allCustomerData[i][1] + "\n"
+           txtaCustomer2.value = message
+    } else {
+        // transit error
+        lblMessage2 = `Error: ${req.status}`
+    }  
 }
 
+
+btnDelete.onclick = function() {
+  let customerDel = inptCustomerDel.value
+  let found = false
+  for (i = 0; i < allCustomerData.length; i++) {
+    if (customerDel == allCustomerData[i][1]) {
+      found = true
+      break
+    }
+  }
+  if (found == false)
+    lblMessage2.textContent = "That customer name is not in the database."
+  else if (found == true) {
+    query = "DELETE FROM customers WHERE name = '" + customerDel + "'"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+    if (req.status == 200) //transit worked.
+      if (req.responseText == 500)
+        lblMessage2.textContent = `You have successfully deleted the customer named ${customerDel}`
+    else
+      lblMessage2.textContent = `There was a problem deleting ${customerDel} from the database.`
+    else
+      lblMessage2.textContent = `Error: ${req.status}`
+  }
+}
