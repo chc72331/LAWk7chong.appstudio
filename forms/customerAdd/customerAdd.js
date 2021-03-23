25c9522ec88085d912fa39861050e3b0335fa845
chc72331 <chc72331@creighton.edu>
2021-03-22 23:31:35 -0500

    Update customerAdd.js

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -1,9 +1,9 @@
 btnCustomerAdd.onclick = function() {
-  let name = inptNewName.value
-  let address = inptNewAddress.value
-  let city = inptNewCity.value
-  let state = inptNewState.value
-  let zipcode = inptNewZip.value
+  let name = inptName.value
+  let address = inptAddress.value
+  let city = inptCity.value
+  let state = inptState.value
+  let zipcode = inptZipcode.value
   let query = "INSERT INTO customers (`name`, `street`, `city`, `state`, `zipcode`) VALUES ('" + name + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
   if (req.status == 200) {
